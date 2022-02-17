24f75b39d4c61c2719196e80aafb1ae54cde4fd5
Williane <williane.tads@gmail.com>
2022-02-17 19:16:31 -0300

    adicionando chamada a api do stripe e utilizando server side rendering

---
+++ src/services/stripe.ts
@@ -0,0 +1,15 @@
+import Stripe from 'stripe';
+import infos from '../../package.json';
+
+export const stripe = new Stripe(
+
+    process.env.STRIPE_API_KEY,
+    {
+        apiVersion: '2020-08-27',
+        appInfo: {
+            name: 'Ignews',
+            version: infos.version,
+        },
+        
+    }
+);
