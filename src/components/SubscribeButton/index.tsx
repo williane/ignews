40cb58ef48e63643cac1c05447c6edf99fc7bdf6
Williane <williane.tads@gmail.com>
2022-02-18 17:48:28 -0300

    redirecionando para pagina de pagamentos do stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -8,10 +11,20 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
+    }
+
+    try {
+      const response = await api.post(`/subscribe`);
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (e) {
+      console.log(e);
     }
   }
 
