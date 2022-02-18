9cb3dccb7b7e82484de9139351681fbaa1afd676
Williane <williane.tads@gmail.com>
2022-02-18 14:46:21 -0300

    adicionando autenticação com next auth e github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { Header } from "../components/Header";
 
 import "../styles/global.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface AppSessionProps extends AppProps {
+  session: any;
+}
+
+function MyApp({ session, Component, pageProps }: AppSessionProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
