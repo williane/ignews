af21dbe28df56e8143b61665d9164fe88bb0fa98
Williane <williane.tads@gmail.com>
2022-02-19 21:55:55 -0300

    configurando navegação entre as paginas com o componete novo ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { SignInButton } from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -7,10 +8,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a href="" className={styles.active}>
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
