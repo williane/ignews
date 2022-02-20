66ae325dab9d36a1091b534fc1c45e0a6f93af51
Williane <williane.tads@gmail.com>
2022-02-20 12:36:50 -0300

    validando se existe uma assinatura valida

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,6 +40,16 @@ export const getServerSideProps: GetServerSideProps = async ({
   params,
 }) => {
   const session = await getSession({ req });
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   const { slug } = params;
 
   const prismic = getPrismicClient(req);
