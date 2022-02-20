b7c882532e1d2b8d29c6cdffe4f44ca78ac7abf8
Williane <williane.tads@gmail.com>
2022-02-20 16:06:12 -0300

    analisando erro 500 na rota de subscription

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -45,23 +45,27 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
       customerId = stripeCustomer.id;
     }
 
-    const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: customerId,
-      payment_method_types: ["card"],
-      billing_address_collection: "required",
-      line_items: [
-        {
-          price: "price_1KUD8wLba5MH7mSumOP4pQbI",
-          quantity: 1,
-        },
-      ],
-      mode: "subscription",
-      allow_promotion_codes: true,
-      success_url: process.env.STRIPE_SUCCESS_URL,
-      cancel_url: process.env.STRIPE_CANCEL_URL,
-    });
+    try {
+      const stripeCheckoutSession = await stripe.checkout.sessions.create({
+        customer: customerId,
+        payment_method_types: ["card"],
+        billing_address_collection: "required",
+        line_items: [
+          {
+            price: "price_1KUD8wLba5MH7mSumOP4pQbI",
+            quantity: 1,
+          },
+        ],
+        mode: "subscription",
+        allow_promotion_codes: true,
+        success_url: process.env.STRIPE_SUCCESS_URL,
+        cancel_url: process.env.STRIPE_CANCEL_URL,
+      });
+    } catch (error) {
+      response.json({ error: error.message });
+    }
 
-    response.status(200).json({ sessionId: stripeCheckoutSession.id });
+    response.status(200).json({ sessionId: "stripeCheckoutSession.id" });
   } else {
     response.setHeader("allow", "POST");
     response.status(405).end("Method not allowed");
